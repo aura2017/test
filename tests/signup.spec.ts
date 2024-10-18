f9b6f6d56ab809182bb1eadbee9156c517719f7c
NatDiazAnthology <natalia.diaz@anthology.com>
2024-10-18 16:10:35 -0500

    signup

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -17,7 +17,9 @@ test('TC 2 Click on Sign up', async ({ page }) => {
   await page.goto('https://tdm2-demo.blackboard.com/webapps/consulting-central/app/launch/TDMv2?locale=en-US');
   await page.waitForTimeout(10000);
   await page.getByText('Sign up').click();
-  await page.waitForTimeout(10000);
+  //await page.waitForTimeout(60000);
+  
   const tdm = await page.getByText('Sign Up');
   await expect(tdm).toBeVisible();
-});
+  
+}); 
