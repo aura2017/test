9ede10d4711eaa3a8c6c234c7a62624993de6541
NatDiazAnthology <natalia.diaz@anthology.com>
2024-10-18 15:55:58 -0500

    signup

---
+++ tests/signup.spec.js
@@ -0,0 +1,23 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('TC1: Open Page', async ({ page }) => {
+  await page.goto('https://tdm2-demo.blackboard.com/webapps/consulting-central/app/launch/TDMv2?locale=en-US');
+  await page.waitForTimeout(10000);
+ 
+
+  //await page.getByRole('link', { name: 'presentation' }).click()
+
+  const tdm = await page.getByText('sign up');
+  await expect(tdm).toBeVisible();
+
+});
+
+test('TC 2 Click on Sign up', async ({ page }) => {
+  await page.goto('https://tdm2-demo.blackboard.com/webapps/consulting-central/app/launch/TDMv2?locale=en-US');
+  await page.waitForTimeout(10000);
+  await page.getByText('Sign up').click();
+  await page.waitForTimeout(10000);
+  const tdm = await page.getByText('Sign Up');
+  await expect(tdm).toBeVisible();
+});
